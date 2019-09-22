bec76fe145c61677b368208727405140cb03782b
Laura Berge <lauraeberge@gmail.com>
2019-09-22 13:55:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,7 +19,7 @@ class Dog
   
   def self.drop_table
     sql = <<-SQL
-      DROP TABLE IF NOT EXISTS dogs (
+      DROP TABLE IF EXISTS dogs (
       id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT)
