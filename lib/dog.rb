bf32d8e8a8fa499657390eee8bca878650692b36
Laura Berge <lauraeberge@gmail.com>
2019-09-22 14:25:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,8 +68,12 @@ class Dog
     dog
   end
   
-  def self.find_by_name
-    
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+      SQL
+    DB[:conn].execute(sql, name)
   end
   
   def update
