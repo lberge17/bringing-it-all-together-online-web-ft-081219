8e7f84d5ccb366b136ab1d3810f132277ef620c4
Laura Berge <lauraeberge@gmail.com>
2019-09-22 14:23:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -56,7 +56,7 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     sql = <<-SQL
       SELECT * FROM dogs
-      WHERE name = ?, breed = ?
+      WHERE name = ? AND breed = ?
       SQL
     dog = DB[:conn].execute(sql, name, breed)
     if dog.empty?
