18ad84a127462f3c99330c760c87d792ca49b1e4
Laura Berge <lauraeberge@gmail.com>
2019-09-22 13:55:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,7 +18,13 @@ class Dog
   end
   
   def self.drop_table
-    
+    sql = <<-SQL
+      DROP TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT)
+      SQL
+    DB[:conn].execute(sql)
   end
   
   def save
